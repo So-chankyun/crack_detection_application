3213655b3ab3aa1efd98621cc192b901d13a1972
Sochankyun <chankyun97@gmail.com>
2022-02-18 17:29:56 +0900

    add identify user, store image. And client is able to download result

--- crack_detection/detection/routes.py
+++ crack_detection/detection/routes.py
@@ -9,17 +9,30 @@ from tqdm import tqdm
 from collections import namedtuple
 import shutil
 
+def get_client_info():
+    APP_ROOT_PATH = current_app.root_path
+
+    # ip 주소 저장
+    client_ip = request.environ.get('HTTP_X_REAL_IP',request.remote_addr).replace('.','')
+    client_folder = os.path.join(APP_ROOT_PATH,'static',client_ip)
+
+    return client_ip, client_folder
+
 detection = Blueprint('detection', __name__)
 
 @detection.route("/img_predict", methods=['GET','POST'])
 def img_predict():
+
+    client_ip, client_folder = get_client_info()
+    
     # 경로지정
-    img_folder = os.path.join(current_app.root_path,'static/true/img')
-    pred_folder = os.path.join(current_app.root_path,'static/pred/img')
+    img_folder = os.path.join(client_folder,'true/img')
+    pred_folder = os.path.join(client_folder,'pred/img')
 
     # 예측
     print('predict')
     img_list = os.listdir(img_folder)
+    print(img_list)
     ext = os.path.splitext(img_list[0])[1]
     img_name_list = [os.path.splitext(file_name)[0]  for file_name in img_list]
     img_path_list = [os.path.join(img_folder,file_name) for file_name in img_list]
@@ -31,17 +44,22 @@ def img_predict():
     pred_path = []
     for i in range(len(img_list)):
         pred_path.append(os.path.join(pred_folder,img_name_list[i]+'_pred'+ext))
-        print(pred)
         pred[i].save(pred_path[i])
+        print(pred_path[i])
 
     # url_for 객체를 저장한다.
     result = namedtuple('result',['crack_image','pred_image'])
     result_url = []
 
     for name in img_name_list:
-        img_file = url_for('static',filename='/true/img/'+name+ext)
-        pred_file = url_for('static',filename='/pred/img/'+name+'_pred'+ext)
-        result_url.append(result(img_file,pred_file))
+        # img_file_name = os.path.join(client_ip,'true/img/'+name+ext)
+        # pred_file_name = os.path.join(client_ip,'pred/img/'+name+'_pred'+ext)
+        # print('img_file_name : {}'.format(img_file_name))
+
+        img_file_url = url_for('static',filename=client_ip+'/true/img/'+name+ext)
+        pred_file_url = url_for('static',filename=client_ip+'/pred/img/'+name+'_pred'+ext)
+        print(img_file_url)
+        result_url.append(result(img_file_url,pred_file_url))
 
     data = {'result_list':result_url}
 
@@ -54,12 +72,14 @@ def img_download():
     # db에서 유저를 구별하여 해당 폴더를 다운로드 할수 있도록 해야한다.
     # db구성을 어떻게 해야하나...
     # 일단은 해당 폴더를 지정하여 저장이되는지 한번 살펴보자
-    path = os.path.join(current_app.root_path,"static/pred/")
-    print(path)
-    # zip file 저장
-    shutil.make_archive(path+'pred','zip',path+'img') 
 
-    return send_file(os.path.join(current_app.root_path,path,'pred.zip'),
+    client_ip, client_folder = get_client_info()
+    
+    # zip file 저장
+    zip_store_path = client_folder+'_result'
+    shutil.make_archive(zip_store_path,'zip',client_folder)  # 저장하고자하는 이름, 확장자, 해당 폴더
+
+    return send_file(zip_store_path+'.zip',
                     attachment_filename="pred.zip",
                     as_attachment=True)
 
--- crack_detection/image/routes.py
+++ crack_detection/image/routes.py
@@ -3,6 +3,7 @@ from flask import (render_template, request, Blueprint
 from crack_detection.image.forms import TargetForm
 from PIL import Image
 import os
+import shutil
 
 image = Blueprint('image',__name__)
 
@@ -15,14 +16,26 @@ def input():
         # 그리고 predict로 이동
 
         img_files = form.crack_image.data
-        print(img_files)
 
-        # 일단은 저장하지말고 바로 predict로 보내보자.
         if img_files:
             # 일단은 원본으로 저장하자. 어차피 전처리 단계에서 모델에 맞는 크기로 변경한다.
             # test code가 이미지의 경로로 데이터를 load하기 때문에 이렇게 해야한다.
+            APP_ROOT_PATH = current_app.root_path
+
+            # ip 주소 저장
+            client_ip = request.environ.get('HTTP_X_REAL_IP',request.remote_addr).replace('.','')
+            client_folder = os.path.join(APP_ROOT_PATH,'static',client_ip)
+            
+            # ip주소로 이루어진 폴더가 존재하는가?
+            if os.path.exists(client_folder):
+                shutil.rmtree(client_folder,ignore_errors=True)
+
+            os.mkdir(client_folder)
+            os.makedirs(client_folder+'/true/img')
+            os.makedirs(client_folder+'/pred/img')
+
             for img in img_files:
-                store_path = os.path.join(current_app.root_path,'static/true/img',img.filename)
+                store_path = os.path.join(client_folder,'true/img',img.filename)
                 Image.open(img).save(store_path)
 
             flash("File successfully uploaded")
